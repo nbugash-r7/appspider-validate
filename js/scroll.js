2a6f97ad6ed782b25e732a48dd08a9a543cb39a3
Nonico Bugash <nonico_bugash@rapid7.com>
2016-02-19 17:38:09 -0500

    Fixed scrolling effect of sidebar

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
 	$(".scroll-to-attack-request").click(function () {
-			$('html,body').animate({scrollTop: 1000}, 2000);
+		console.log("scrolling to attack request");
+		$('html,body,div#main-wrapper').animate({scrollTop: 33}, 'slow');
 		});
 	$('.scroll-to-attack-payload').click(function () {
-			$('html,body').animate({scrollTop: '+=200px'}, 2000);
+			console.log("scrolling to attack payload");
+			$('html,body,div#main-wrapper').animate({scrollTop: 467}, 'slow');
 		});
 	$(".scroll-to-attack-response").click(function () {
-			$('html,body').animate({scrollTop: '+=300px'}, 2000);
+			console.log("scrolling to attack response");
+			$('html,body,div#main-wrapper').animate({scrollTop: 590}, 'slow');
 		});
 	$(".scroll-to-attack-content").click(function(){
-			$('html,body').animate({scrollTop: '+=1000'}, 2000);
+			console.log("scrolling to attack content");
+			$('html,body,div#main-wrapper').animate({scrollTop: $(document).height()}, 'slow');
 		});
 });
 
